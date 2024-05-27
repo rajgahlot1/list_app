36552be99b04520fc9de4f96f49115bf45268630
rajgahlot1 <rajcommando1.38@gmail.com>
2024-05-27 22:33:36 +0530

    css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './list';
+import App from './list.jsx';
+import './list.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
